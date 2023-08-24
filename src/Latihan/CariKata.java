cbf0b6fd9431fea7a7b563935e9744e403770eed
Hisyam Arief Zulfani <zoelfanisyam@gmail.com>
2023-08-24 19:06:52 +0700

    change file CariKata.java

--- src/Latihan/CariKata.java
+++ src/Latihan/CariKata.java
@@ -10,25 +10,23 @@ public class CariKata {
     int mid;
 
     void searchWord(String cari) {
-        // while (left <= right && hasil == 0) {
-        // mid = (left + right) / 2;
-        // if (cari.equals(kamus[mid])) {
-        // hasil = 1;
-        // } else {
-        // right = mid - 1;
-        // }
-        // }
-        // if (hasil == 1) {
-        // System.out.println("Data ditemukan yaitu : " + cari);
-        // } else {
-        // System.out.println("Data tidak ditemukan");
-        // }
-        for (int i = 0; i < kamus.length; i++) {
-            if (cari.equalsIgnoreCase(kamus[i])) {
-                System.out.println("Kata ditemukan yaitu : " + cari);
+        while (left <= right && hasil == 0) {
+            mid = (left + right) / 2;
+
+            int comparisson = cari.compareToIgnoreCase(kamus[mid]);
+
+            if (comparisson == 0) {
+                hasil = 1;
+            } else if (comparisson > 0) {
+                left = mid + 1;
             } else {
-                System.out.println("Kata tidak ditemukan");
+                right = mid - 1;
             }
+        }
+        if (hasil == 1) {
+            System.out.println("Data ditemukan yaitu : " + cari);
+        } else {
+            System.out.println("Data tidak ditemukan");
         }
     }
 
